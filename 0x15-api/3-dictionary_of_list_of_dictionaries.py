0a6458f9bf2c2c6274f0875ff199fe3a180f2ec5
Tr3v1n4t0r <813@holbertonschool.com>
2020-05-05 12:04:54 -0700

    3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+For all employees, export to JSON information about TODO list progress
+"""
+
+if __name__ == '__main__':
+    import requests
+    import json
+    from sys import argv
+
+    all_id = set()
+    req = requests.get('https://jsonplaceholder.typicode.com/posts')
+    data = req.json()
+    for user in data:
+        all_id.add(user.get('userId'))
+
+    export = {}
+    for user in all_id:
+        req = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                       format(user))
+    username = req.json().get('username')
+
+    req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                       format(argv[user]))
+    data = req.json{}
+
+    export['{}'.format(argv[1])] = []
+    for task in data:
+        export['{}'.format(argv[1])].append({
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': username
+        })
+
+    with open('todo_all_employees.json', 'w') as outfile:
+        json.dump({int(x): export[x] for x in export.keys()},
+                  outfile, sort_keys=True)
